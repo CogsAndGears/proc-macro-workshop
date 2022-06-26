f8b37558d610df43ba644f5f744c7993c67f24aa
cogs <cogs@springless.com>
2022-06-26 10:39:22 -0700

    Pass builder test 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -3,16 +3,34 @@ use syn::{
     parse_macro_input,
     DeriveInput,
 };
-use quote::{ quote };
+use quote::{ quote, format_ident };
 
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let _ = quote! {
-        #input
+    let type_name = input.ident;
+    let command_type_name = format_ident!("{}Builder", type_name);
+    let expanded = quote! {
+        pub struct #command_type_name {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_div: Option<String>,
+        }
+
+        impl #type_name {
+            pub fn builder() -> #command_type_name {
+                #command_type_name {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_div: None,
+                }
+            }
+        }
     };
 
-    TokenStream::new()
+    TokenStream::from(expanded)
 }
